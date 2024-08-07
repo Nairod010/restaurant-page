f681a4414aa5f275a45ac35b94ca51aabbb9ecce
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-08-07 09:14:19 +0300

    in progress about page

--- src/about-page.js
+++ src/about-page.js
@@ -1,7 +1,24 @@
 function aboutPage(){
     const div = document.querySelector("#content");    
+    
+    const aboutHeader = document.createElement("h2")
+    const aboutUs = document.createElement("p")
+    const aboutContact = document.createElement("p")
 
-    div.textContent = "";
+    aboutUs.classList.add("about-text")
+    aboutUs.textContent = "At Meloons, it's not just about the food – it's about the experience. Our friendly staff, beach-themed décor, and upbeat atmosphere make Meloons the perfect spot for any occasion. Whether you're celebrating a special event, enjoying a casual meal with friends, or just looking for a place to kick back and relax, you'll feel right at home at Meloons."
+    
+    aboutHeader.classList.add("about-header")
+    aboutHeader.textContent = "About Meloons"
+
+    aboutContact.classList.add("about-contact")
+    aboutContact.textContent = ""
+
+    div.textContent = "Have questions or need more information? Feel free to reach out to us at realmail@fake.com. We'd love to hear from you!";
+
+    div.appendChild(aboutHeader)
+    div.appendChild(aboutUs)
+    div.appendChild(aboutContact)
 }
 
 export {
